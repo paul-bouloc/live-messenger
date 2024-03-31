c938476b7efb63c4db3f8fb668c3112cc8047a52
moicestpaul <bouloc.p@gmail.com>
2024-04-01 00:55:31 +0200

    Fix errorCodes

--- api/src/constants/errorCodes.ts
+++ api/src/constants/errorCodes.ts
@@ -6,14 +6,14 @@ export type ErrorCode = {
 
 const errorCodes = {
     // User
-    USER_NOT_FOUND: {name: 'UserError', message: 'User not found', statusCode: 404},
-    USER_ALREADY_EXISTS: {name: 'UserError', message: 'User already exists', statusCode: 409},
+    USER_NOT_FOUND: <ErrorCode>{name: 'UserError', message: 'User not found', statusCode: 404},
+    USER_ALREADY_EXISTS: <ErrorCode>{name: 'UserError', message: 'User already exists', statusCode: 409},
 
     // Auth
-    UNAUTHORIZED: {name: 'AuthError', message: 'Unauthorized', statusCode: 401},
-    INVALID_TOKEN: {name: 'AuthError', message: 'Invalid token', statusCode: 401},
-    INVALID_CREDENTIALS: {name: 'AuthError', message: 'Invalid credentials', statusCode: 401},
-    TOKEN_EXPIRED: {name: 'AuthError', message: 'Token expired', statusCode: 401},
+    UNAUTHORIZED: <ErrorCode>{name: 'AuthError', message: 'Unauthorized', statusCode: 401},
+    INVALID_TOKEN: <ErrorCode>{name: 'AuthError', message: 'Invalid token', statusCode: 401},
+    INVALID_CREDENTIALS: <ErrorCode>{name: 'AuthError', message: 'Invalid credentials', statusCode: 401},
+    TOKEN_EXPIRED: <ErrorCode>{name: 'AuthError', message: 'Token expired', statusCode: 401},
 
 }
 
