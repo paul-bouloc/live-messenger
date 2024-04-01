6899b2b120e37434378ec635c081d5a0d07e0334
moicestpaul <bouloc.p@gmail.com>
2024-04-01 18:23:39 +0200

    API: Register now login too

--- api/src/controllers/authController.ts
+++ api/src/controllers/authController.ts
@@ -1,40 +1,46 @@
-import { AppError } from "@/models/appError"
-import errorCodes from "@/constants/errorCodes"
-import userService from "@/services/userService"
-import bcrypt from 'bcrypt'
-import AuthService from "@/services/authService"
+import { AppError } from "@/models/appError";
+import errorCodes from "@/constants/errorCodes";
+import userService from "@/services/userService";
+import AuthService from "@/services/authService";
 
 export const register = async (req, res, next) => {
+	const { name, email, password } = req.body;
 
-  const { name, email, password } = req.body
+	const user = await userService.getUserByEmail(email);
 
-  const user = await userService.getUserByEmail(email)
-
-  if (user) {
-    throw new AppError(errorCodes.USER_ALREADY_EXISTS)
-  }else{
-    const newUser = await userService.createUser(name, email, password)
-    return res.status(201).json({message: 'User created', user: newUser})
-  }
-
-}
+	if (user) {
+		throw new AppError(errorCodes.USER_ALREADY_EXISTS);
+	} else {
+		const newUser = await userService.createUser(name, email, password);
+		const token = AuthService.generateJwtToken(newUser.id);
+		return res
+			.cookie("jwt", token, {
+				httpOnly: true,
+				signed: true,
+				secure: process.env.NODE_ENV === "production",
+			})
+			.status(201)
+			.json({ message: "User created", user: newUser });
+	}
+};
 
 export const login = async (req, res, next) => {
+	const { email, password } = req.body;
 
-  const { email, password } = req.body
+	const user = await AuthService.login(email, password);
 
-  const user = await AuthService.login(email, password)
+	const token = AuthService.generateJwtToken(user.id);
 
-  const token = AuthService.generateJwtToken(user.id)
-
-  return res
-          .cookie('jwt', token, {httpOnly: true, signed: true, secure: process.env.NODE_ENV === 'production'})
-          .status(200)
-          .json({message: 'Login successful', user})
-
-
-}
+	return res
+		.cookie("jwt", token, {
+			httpOnly: true,
+			signed: true,
+			secure: process.env.NODE_ENV === "production",
+		})
+		.status(200)
+		.json({ message: "Login successful", user });
+};
 
 export const currentUser = async (req, res, next) => {
-  return res.status(200).json(req.user)
-}
+	return res.status(200).json(req.user);
+};
