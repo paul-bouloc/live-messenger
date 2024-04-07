dfb7d8be0bb42333f27fa79ae41158790441c80e
moicestpaul <bouloc.p@gmail.com>
2024-04-07 23:37:00 +0200

    API : Removed commented old code

--- api/src/websocket/handlers/messageHandler.ts
+++ api/src/websocket/handlers/messageHandler.ts
@@ -16,33 +16,3 @@ const messageHandler = (io: Server, socket: Socket) => {
 }
 
 export default messageHandler;
-
-// const messageHandler = (io: Server, socket: Socket) => {
-//   const addMessage = async (payload:{roomId:string, content:string}, callback:CallableFunction) => {
-
-//     messageQueue.add('addMessage', {io, socket, payload});
-
-
-//     try {
-//       const {roomId, content} = payload;
-//       const user = <User>socket.data.user;
-
-//       const room = await RoomService.getRoomById(roomId);
-
-//       if (!room) throw new Error('La conversation n\'existe pas.');
-
-//       const createdMessage = await MessageService.createMessage(roomId, user.id, content);
-//       const message = await MessageService.getMessageById(createdMessage.id);
-
-//       io.to(`room-${roomId}`).emit("message:new", {roomId, message})
-
-//     } catch (error) {
-//       callback(<SocketResponse>{
-//         success: false,
-//         message: error.message || 'Erreur lors de la création de la conversation.'
-//       })
-//     }
-//   }
-
-//   socket.on("message:new", addMessage);
-// }
